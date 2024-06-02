36832b9ff4c26a096bc92dcc5872b5cab51c7e66
MyroslavSB <Myroslavsobchyshyn@gmail.com>
2024-06-02 19:13:08 +0200

    added achievements count get

--- src/app/pages/main/main-page/pages/rankings-page/rankings-page.component.ts
+++ src/app/pages/main/main-page/pages/rankings-page/rankings-page.component.ts
@@ -1,19 +1,61 @@
-import {Component} from '@angular/core';
+import {ChangeDetectionStrategy, ChangeDetectorRef, Component, DestroyRef, inject, OnInit} from '@angular/core';
 import {EUserRoles} from "../../../../../services/user-info/utils/e-user-roles";
 import {NgForOf} from "@angular/common";
 import {IUser} from "../../../../../shared/interfaces/core-models/i-user";
+import {FormsModule} from "@angular/forms";
+import {ERankingOptions} from "../../../../../shared/enums/e-ranking-options";
+import {RankingsApiService} from "../../../../../services/rankings-api/rankings-api.service";
+import {takeUntilDestroyed} from "@angular/core/rxjs-interop";
 
 @Component({
   selector: 'app-rankings-page',
   standalone: true,
   imports: [
-    NgForOf
+    NgForOf,
+    FormsModule
   ],
   templateUrl: './rankings-page.component.html',
-  styleUrl: './rankings-page.component.scss'
+  styleUrl: './rankings-page.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class RankingsPageComponent {
+export class RankingsPageComponent implements OnInit {
 
+  private destroyRef = inject(DestroyRef)
   private userRoles = EUserRoles
+
+  public rankingOptions = ERankingOptions
+  public rankingOption: ERankingOptions = ERankingOptions.VISITS
+
   public users: IUser[] = []
+
+  constructor(
+    private cdRef: ChangeDetectorRef,
+    private rankingsAPI: RankingsApiService
+  ) {
+  }
+
+  ngOnInit() {
+    this.getRankedUsers()
+  }
+
+  public onSelectOptionPick(value: ERankingOptions): void {
+    this.rankingOption = value
+    this.getRankedUsers()
+  }
+
+  private getRankedUsers(): void {
+    this.rankingsAPI.getRankedUsers(this.rankingOption)
+      .pipe(takeUntilDestroyed(this.destroyRef))
+      .subscribe(users => {
+        this.users = users
+        this.cdRef.detectChanges()
+      })
+
+  }
+
+  public get columnTitle(): string {
+    return this.rankingOption === this.rankingOptions.VISITS ? 'Visits count' : 'Achievements count'
+  }
+
+
 }
